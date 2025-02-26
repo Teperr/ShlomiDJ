f8a84e9556320e51d62933cedcdc31de416764a9
teperr <omerteperr@gmail.com>
2025-02-26 19:25:03 +0200

    Update to site add and fix new sections

--- js/controller.js
+++ js/controller.js
@@ -3,4 +3,24 @@
 function sayHi(){
     console.log('HI');
     
-}s
+}
+
+function toggleNav() {
+    let sidebar = document.getElementById("sidebar");
+    let menuToggle = document.getElementById("menu-toggle");
+    let icon = menuToggle.querySelector("i");
+
+    // טוגל לתפריט הצד
+    sidebar.classList.toggle("open");
+
+    // שינוי אייקון כפתור המבורגר
+    if (sidebar.classList.contains("open")) {
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-times"); // X כשפתוח
+    } else {
+        icon.classList.remove("fa-times");
+        icon.classList.add("fa-bars"); // חזרה להמבורגר
+    }
+}
+
+
---
+++ js/menu.js
@@ -0,0 +1,10 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const menuToggle = document.getElementById("menu-toggle");
+//     const socialMenu = document.getElementById("social-menu");
+
+//     menuToggle.addEventListener("click", function () {
+//         socialMenu.classList.toggle("open");
+//         console.log('omer');
+        
+//     });
+// });
